d300c09ddeb70d923b35db21f6a696da4b83244b
tom_nowak <tomasz.nowak.stw@gmail.com>
2018-04-30 01:01:56 +0200

    build_all - creates build directories, rebuild_all - uses exisiting directories

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,9 @@
 rebuild()
 {
     cd $1
+    rm -rf build
+    mkdir build
     cd build
-    rm -rf *
     cmake -DCMAKE_BUILD_TYPE=Debug .. # will be changed to Release
     make -j$(nproc) # number of parallel make jobs = number of CPU cores
     cd ../..

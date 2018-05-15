5317251de41eec9641051b942dcefe582fb3619d
tom_nowak <tomasz.nowak.stw@gmail.com>
2018-05-15 13:23:35 +0200

    adjusted to changes in UDPClient - added default callback; small bugfix in callback class - avoid illegal memory access in case message length == buffer size

--- central/src/main.cpp
+++ central/src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cstring>
 
-class Callback : public common::UDPClient::MessageCallback
+class Callback : public common::UDPClient::Callback
 {
 public:
     Callback(const std::string str)
@@ -11,7 +11,8 @@ public:
 
     virtual void callbackOnReceive(common::Address &address, char *msg, size_t length)
     {
-        msg[length] = '\0';
+        if(length < 512)
+            msg[length] = '\0';
         std::cout << name << " - received: '" << msg << "'\nfrom: ";
         address.print(std::cout);
         std::cout << std::endl;
@@ -33,13 +34,13 @@ int main(int argc, char **argv)
         std::cout << "Usage: " << argv[0] << " <host> <port1> <port2>\n";
         return -1;
     }
-    common::UDPClient client(512);
+    common::Address server_address1(common::AddressInfo(argv[1], argv[2], SOCK_DGRAM).getResult());
+    common::Address server_address2(common::AddressInfo(argv[1], argv[3], SOCK_DGRAM).getResult());
+    Callback callback1("callback1"), callback2("callback2"), default_callback("default callback");
+    common::UDPClient client(512, &default_callback);
     client.getSocket().setSendTimeout(5000);
     client.getSocket().setReceiveTimeout(5000);
     std::cout << "UDP client started\n";
-    common::Address server_address1(common::AddressInfo(argv[1], argv[2], SOCK_DGRAM).getResult());
-    common::Address server_address2(common::AddressInfo(argv[1], argv[3], SOCK_DGRAM).getResult());
-    Callback callback1("callback1"), callback2("callback2");
     client.addToMessageQueue(&callback1, server_address1, "hello1", sizeof("hello1"));
     client.addToMessageQueue(&callback2, server_address2, "hello2", sizeof("hello2"));
     std::cout << "Sent messages\n";
